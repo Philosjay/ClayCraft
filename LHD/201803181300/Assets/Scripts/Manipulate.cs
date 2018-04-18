b7ebb909615b72f49e6148fb9f612c99e717a285
Medill-East <NEMailLHD@163.com>
2018-04-18 12:58:56 +0800

    something from lhd
    clay deformation test

---
+++ LHD/201803181300/Assets/Scripts/Manipulate.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[AddComponentMenu("Modifiers/FFD/FFD Manipulate")]
+public class Manipulate : MonoBehaviour {
+
+	MegaFFD mega;
+	MegaModifiers modifiers;
+	GameObject clay;
+
+	// Use this for initialization
+	void Start () {
+
+      	clay = GameObject.FindWithTag("Clay");
+        Debug.Log(clay.name);
+        AddFDD();
+		
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void AddFDD()
+	{
+		if(!clay.GetComponent<MegaFFD2x2x2>())
+		{
+            clay.AddComponent<MegaFFD2x2x2>();
+        }
+	}
+
+}
