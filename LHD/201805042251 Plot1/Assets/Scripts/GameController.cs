3f989592acda88873c03e786bd6bbd163116572d
Medill-East <NEMailLHD@163.com>
2018-05-04 22:52:27 +0800

    Plot1
    201805042251

---
+++ LHD/201805042251 Plot1/Assets/Scripts/GameController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour {
+
+    VedioController vedioController;
+    LightController lightController;
+    UIController uiController;
+
+	// Use this for initialization
+	void Start () {
+        lightController = GameObject.FindGameObjectWithTag("GameController").GetComponent<LightController>();
+        vedioController = GameObject.FindGameObjectWithTag("GameController").GetComponent<VedioController>();
+        uiController = GameObject.FindGameObjectWithTag("GameController").GetComponent<UIController>();
+
+        uiController.TurnOffTextPanel();
+        lightController.TurnOffLight();
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+        CheckClickAndFade();
+	}
+
+    void CheckClickAndFade()
+    {
+        //鼠标点击淡出
+        if (Input.GetMouseButtonDown(0))
+        {
+            Debug.Log("当点击屏幕的时候可以触发跳转场景等一些事件");
+            vedioController.FadeOut();
+            StartCoroutine(lightController.WaitAndTurnOnTheLight(3.0f));
+            StartCoroutine(uiController.WaitAndPrint(6.0f));
+        }
+    }
+}
---
+++ LHD/201805042251 Plot1/Assets/Scripts/UIController.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+
+public class UIController : MonoBehaviour {
+
+    private GameObject plot1;
+    private Text plotText;
+
+    //打字机效果相关
+    public float letterPause = 0.2f;
+    //private AudioSource printAudio;
+    private string tempWord;
+    private string text = "我是谁？你知道我的名字吗？";
+    //public AudioClip printClip;
+    
+
+	// Use this for initialization
+	void Start () {
+        plot1 = GameObject.FindGameObjectWithTag("PlotTextPanel");
+        plotText = plot1.GetComponentInChildren<Text>();
+
+        tempWord = text;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void TurnOnTextPanel()
+    {
+        plot1.SetActive(true);
+        plotText.DOText("我是谁？你知道我的名字吗？", 3);
+    }
+
+    public void TurnOffTextPanel()
+    {
+        plot1.SetActive(false);
+    }
+
+    //一段时间后显示文字
+    public IEnumerator WaitAndPrint(float waitTime)
+    {
+        //yield return new WaitForSeconds(waitTime);
+        //plot1.SetActive(true);
+        //plotText.DOText("我是谁？", 3);
+        //plotText.DOText("我是谁？你知道我的名字吗？", 5);
+
+        yield return new WaitForSeconds(waitTime);
+        plot1.SetActive(true);
+
+        foreach (char letter in tempWord.ToCharArray())
+        {
+            plotText.text += letter;
+            //if (clip)
+            //{
+            //    source.PlayOneShot(clip);
+            //}
+            yield return new WaitForSeconds(letterPause);
+        }
+    }
+
+
+}
