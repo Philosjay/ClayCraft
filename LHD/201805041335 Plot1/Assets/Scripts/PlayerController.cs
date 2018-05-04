71f8dfd6b23c96de7948f2f8df0fe9b4c2ce3b14
Medill-East <NEMailLHD@163.com>
2018-05-04 13:37:04 +0800

    Plot1
    201805041335

---
+++ LHD/201805041335 Plot1/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+namespace UnityStandardAssets.Characters.ThirdPerson
+{
+    public class PlayerController : MonoBehaviour
+    {
+
+        public GameObject player;
+        public ThirdPersonUserControl userControl;
+
+        // Use this for initialization
+        void Start()
+        {
+            player = GameObject.FindGameObjectWithTag("Player");
+            userControl = player.GetComponent<ThirdPersonUserControl>();
+            userControl.enabled = false;
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
+
---
+++ LHD/201805041335 Plot1/Assets/Scripts/VedioController.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+using DG.Tweening;
+
+public class VedioController : MonoBehaviour {
+
+    GameObject mainCamera;
+    AudioSource mainSource;
+    GameObject audioController;
+    VideoPlayer vedioPlayer;
+
+    // Use this for initialization
+    void Start () {
+        mainCamera = GameObject.FindGameObjectWithTag("MainCamera");
+        mainSource = mainCamera.GetComponent<AudioSource>();
+        audioController = GameObject.Find("Plot Vedio Controller");
+        vedioPlayer = audioController.GetComponent<VideoPlayer>();
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            Debug.Log("当点击屏幕的时候可以触发跳转场景等一些事件");
+            FadeOut();       
+        }
+    }
+
+    void FadeOut()
+    {
+        Debug.Log("fade");
+        DOTween.To(() => vedioPlayer.targetCameraAlpha, x => vedioPlayer.targetCameraAlpha = x, 0, 1);
+        DOTween.To(() => mainSource.volume, x => mainSource.volume = x, 0, 1);
+
+        StartCoroutine(WaitAndStop(1.0f));
+
+        Debug.Log(vedioPlayer.targetCamera);
+        Debug.Log(vedioPlayer.targetCameraAlpha);
+    }
+
+    IEnumerator WaitAndStop(float waitTime)
+    {
+        yield return new WaitForSeconds(waitTime);
+        vedioPlayer.Stop();
+    }
+
+
+}
