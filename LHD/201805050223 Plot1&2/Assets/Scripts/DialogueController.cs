dde20382d0883aee148643bfe34d4e433b15017a
Medill-East <NEMailLHD@163.com>
2018-05-05 02:26:04 +0800

    Plot1&2
    201805050223

---
+++ LHD/201805050223 Plot1&2/Assets/Scripts/DialogueController.cs
@@ -0,0 +1,117 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogueController : MonoBehaviour {
+
+    private GameObject plot1;
+    private Text plotText;
+
+    [System.Serializable]
+    public enum dialogueText
+    {
+        我是谁,
+        你知道我的名字吗,
+        我感觉身体很痛,
+        你能帮助我重塑身体吗
+     }
+
+    //打字机效果相关
+    public float letterPause = 0.2f;
+    //private AudioSource printAudio;
+    public int currentDialogue = 1;
+    private string tempWord;
+    private string text = "我是谁?你知道我的名字吗?";
+    //public AudioClip printClip;
+
+    // Use this for initialization
+    void Start () {
+        plot1 = GameObject.FindGameObjectWithTag("PlotTextPanel");
+
+        plotText = plot1.GetComponentInChildren<Text>();
+
+        tempWord = text;
+    }
+
+    //一段时间后显示文字
+    public IEnumerator WaitAndPrint(float waitTime)
+    {
+        yield return new WaitForSeconds(waitTime);
+        plot1.SetActive(true);
+
+        switch (currentDialogue)
+        {
+            case 1:          
+                tempWord = dialogueText.我是谁.ToString();
+                foreach (char letter in tempWord.ToCharArray())
+                {
+                    plotText.text += letter;
+                    //if (clip)
+                    //{
+                    //    source.PlayOneShot(clip);
+                    //}
+                    yield return new WaitForSeconds(letterPause);
+                }
+                plotText.text = "我是谁?";
+                currentDialogue += 1;
+                goto case 2;
+                
+            case 2:
+                yield return new WaitForSeconds(1.0f);
+                tempWord = dialogueText.你知道我的名字吗.ToString();
+                foreach (char letter in tempWord.ToCharArray())
+                {
+                    plotText.text += letter;
+                    //if (clip)
+                    //{
+                    //    source.PlayOneShot(clip);
+                    //}
+                    yield return new WaitForSeconds(letterPause);
+                }
+                plotText.text = "我是谁?你知道我的名字吗?";
+                currentDialogue += 1;
+                break;
+            case 3:
+                tempWord = dialogueText.我感觉身体很痛.ToString();
+                foreach (char letter in tempWord.ToCharArray())
+                {
+                    plotText.text += letter;
+                    //if (clip)
+                    //{
+                    //    source.PlayOneShot(clip);
+                    //}
+                    yield return new WaitForSeconds(letterPause);
+                }
+                plotText.text = "我感觉身体很痛...";
+                currentDialogue += 1;
+                goto case 4;
+            case 4:
+                yield return new WaitForSeconds(1.0f);
+                tempWord = dialogueText.你能帮助我重塑身体吗.ToString();
+                foreach (char letter in tempWord.ToCharArray())
+                {
+                    plotText.text += letter;
+                    //if (clip)
+                    //{
+                    //    source.PlayOneShot(clip);
+                    //}
+                    yield return new WaitForSeconds(letterPause);
+                }
+                plotText.text = "我感觉身体很痛...你能帮助我重塑身体吗?";
+                currentDialogue += 1;
+                break;
+            default:
+                break;
+        }
+        //foreach (char letter in tempWord.ToCharArray())
+        //{
+        //    plotText.text += letter;
+        //    //if (clip)
+        //    //{
+        //    //    source.PlayOneShot(clip);
+        //    //}
+        //    yield return new WaitForSeconds(letterPause);
+        //}
+    }
+}
---
+++ LHD/201805050223 Plot1&2/Assets/Scripts/GameController.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour {
+
+    VedioController vedioController;
+    LightController lightController;
+    UIController uiController;
+    DialogueController dialogueController;
+
+    private int currentPlot = 1;
+
+	// Use this for initialization
+	void Start () {
+        lightController = GameObject.FindGameObjectWithTag("GameController").GetComponent<LightController>();
+        vedioController = GameObject.FindGameObjectWithTag("GameController").GetComponent<VedioController>();
+        uiController = GameObject.FindGameObjectWithTag("GameController").GetComponent<UIController>();
+        dialogueController = GameObject.FindGameObjectWithTag("GameController").GetComponent<DialogueController>();
+
+        StartCoroutine(WaitAndStart(0.1f));
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+        ShowTime();
+	}
+
+    IEnumerator WaitAndStart(float waitTime)
+    {
+        yield return new WaitForSeconds(waitTime);
+
+        uiController.TurnOffTextPanel();
+        lightController.TurnOffLight();
+    }
+
+    void ShowTime()
+    {
+        //Debug.Log(currentPlot);
+        switch (currentPlot)
+        {
+            case 1:
+                Plot1();
+                break;
+            case 2:
+                Plot2();
+                break;
+            case 3:
+                break;
+            default:
+                break;
+        }
+    }
+
+    void Plot1()
+    {
+        //鼠标点击淡出
+        if (Input.GetMouseButtonDown(0))
+        {
+            Debug.Log("当点击屏幕的时候可以触发跳转场景等一些事件");
+            vedioController.FadeOut();
+            StartCoroutine(lightController.WaitAndTurnOnTheLight(3.0f));
+            //StartCoroutine(dialogueController.WaitAndPrint(6.0f));
+            currentPlot += 1;
+            Debug.Log(currentPlot);
+        }
+    }
+
+    void Plot2()
+    {
+        Debug.Log(dialogueController.currentDialogue);
+        StartCoroutine(dialogueController.WaitAndPrint(6.0f));
+        currentPlot += 1;
+
+    }
+
+
+}
