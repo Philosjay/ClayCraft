0846a623138757813a1d2f29f6ed1535bfb2ea16
Medill-East <NEMailLHD@163.com>
2018-05-05 17:34:22 +0800

    Plot2
    201805051732

---
+++ LHD/201805051732 Plot2/Assets/Scripts/UIController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+
+public class UIController : MonoBehaviour {
+
+    private GameObject plot1;
+
+    // Use this for initialization
+    void Start () {
+        plot1 = GameObject.FindGameObjectWithTag("PlotTextPanel");
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void TurnOnTextPanel()
+    {
+        plot1.SetActive(true);
+        //plotText.DOText("我是谁？你知道我的名字吗？", 3);
+    }
+
+    public void TurnOffTextPanel()
+    {
+        plot1.SetActive(false);
+    }
+
+
+
+
+
+}
